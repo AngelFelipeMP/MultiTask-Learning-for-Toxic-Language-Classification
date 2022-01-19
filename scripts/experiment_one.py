0d6d86f5bf6917146dd18b5eeec4fee5e8f0ba2c
AngelFelipeMP <angel.magnossao@gmail.com>
2022-01-19 15:08:56 +0000

    "changes in experiment_one.py"

--- scripts/experiment_one.py
+++ scripts/experiment_one.py
@@ -1,16 +1,13 @@
 
-data_path = 'data'
+foldes_number = 10
+data_path = '/data'
 path = '/content/drive/MyDrive/Code/MTL_2021/machamp/experiment_one'
 repo_path = path + '/MultiTask-Learning-for-Toxic-Language-Classification'
 results_path = path + '/E1_results'
 
 #MTL
-## train
-dataset_config = repo_path + '/config/EXIST_DETOXIS_config.json'
+## crossvalidation
+dataset_config = repo_path + '/config/E1_mtl_config.json'
 device = 0  #gpu 0 / cpu 1
 output_train = repo_path + 'E1/mtl'
-## test
-model_weights = repo_path + '/logs/E1/' ???????????? '/model.tar.gz'
-dataset_test = '/content/data/EXIST2021_test_with_labeled_processed_[TEST].tsv'
-head =
-device = 0
+
