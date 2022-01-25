9fa4e8791983cdd24f6a00f12374c3309a74bcbc
AngelFelipeMP <angel.magnossao@gmail.com>
2022-01-25 10:27:51 +0000

    "add data_acquisition func and fix process_dada func"

--- scripts/experiment_one.py
+++ scripts/experiment_one.py
@@ -1,10 +1,13 @@
-from utils import split_data, change_parameter_seeds, train
+# from utils import split_data, change_parameter_seeds, train, average
+from utils import process_data, data_acquisition
+import os
 
 folds_number = 1
 experiment = 'E1'
 models = ['mtl', 'stl_exist', 'stl_detoxis']
 device = 0  #gpu 0 / cpu 1
-data_path = '/data'
+source_data_path = '/content/drive/MyDrive/Code/MTL_2021/Data'
+data_path = '/content/data'
 path = '/content/drive/MyDrive/Code/MTL_2021/machamp/experiment_one'
 repo_path = path + '/MultiTask-Learning-for-Toxic-Language-Classification'
 results_path = path + '/E1_results'
@@ -14,13 +17,34 @@ parameter_config = config_path + '_parameter_'
 
 # if __name__ == '__main__':
 
-for fold in range(folds_number):
-    split_data(fold)
-    change_parameter_seeds(parameter_config + 'config.json')
-    for model in models:
-        output_path = experiment + '/' + model 
-        train(
-            dataset_config + model + '_config.json',
-            device,
-            output_path,
-            parameter_config + 'config.json')
+# grab data from drive
+data_acquisition(source_data_path, data_path, ['EXIST','DETOXIS'])
+
+stratify_head_detoxis = process_data(data_path, 'DETOXIS', 'comment', 'toxicity')
+stratify_head_exist = process_data(data_path, 'EXIST', 'text', 'task1')
+
+print(stratify_head_exist)
+print(stratify_head_detoxis)
+
+# for fold in range(folds_number):
+#     split_data(data_path, fold, 0.2, stratify_head_detoxis)
+#     split_data(data_path, fold, 0.2, stratify_head_exist)
+    
+#     change_parameter_seeds(parameter_config + 'config.json')
+#     for model in models:
+#         output_path = experiment + '/' + model 
+#         train(
+#             dataset_config + model + '_config.json',
+#             device,
+#             output_path,
+#             parameter_config + 'config.json')
+
+# average(folds_number, repo_path + '/machamp/logs/' + experiment, models)
+
+## //TODO
+# write the data_split script fuction (utils.py)
+# write the change_parameter_seeds fuction (utils.py)
+# debug process_data
+# debug split_data
+# add randon seed split data
+##### write the fuctions by hand first !!!!!! ########
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,8 +1,75 @@
 import os
+import shutil
+import csv
+from xmlrpc.client import boolean
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
-def split_data(args):
-    #### construction
-    return
+def data_acquisition(source_path=str(), target_folder=str(), datasets=list()):
+    # create a data folder
+    if os.path.exists(target_folder):
+        shutil.rmtree(target_folder)
+    os.makedirs(target_folder)
+    # fetch all files
+    for dataset in datasets:
+        source_folder = source_path + '/' + dataset
+        for file_name in os.listdir(source_folder):
+            # construct full file path
+            source = source_folder + '/' + file_name
+            destination = target_folder + '/' + file_name
+            # copy only files
+            if os.path.isfile(source):
+                shutil.copy(source, destination)
+                print('copied', file_name)
+    
+
+def file_list(path, word_in):
+    csv_list = os.listdir(path)
+    csv_list = [x for x in csv_list if word_in in x]
+    print(f'List of {word_in} files/csv {csv_list}')
+    return csv_list
+
+# def process_data(file_names, path, text_column, label_column, split_test):
+def process_data(path, dataset_name, text_column, label_column):
+    file_names = file_list(path, dataset_name)
+    dataset_info = dict()
+    
+    datasets = {'.csv':[name for name in file_names if '.csv' in name],
+                '.tsv':[name for name in file_names if '.tsv' in name]}
+    
+    for k,v in datasets.items():
+        divide_columns = ',' if k == '.csv' else '\t'
+        for data in v:
+            df = pd.read_csv(path + '/' + data, sep=divide_columns)
+            
+            #remove some "\t" and "\n"
+            df[text_column] = df.loc[:,text_column].apply(lambda x: x.replace('\n', ' '))
+            df[text_column] = df.loc[:,text_column].apply(lambda x: x.replace('\t', ' '))
+            columns_stratify = [label_column, 'language'] if 'language' in df.columns.to_list() else [label_column]
+            head = None if type(df.columns.to_list()[0]) != int else True
+            
+            # save as .tsv
+            df.to_csv(path + '/' + data[:-4] + '_processed' + '.tsv', sep="\t")
+            
+            # save columns to using in the data split for stratification and header None/True
+            dataset_info[data[:-4] + '_processed' + '.tsv'] = [columns_stratify, head]
+            
+    return dataset_info
+            
+
+
+def split_data(data_path=str(), fold_number=int(), test_size=float(), dataset_info=dict()):
+    
+    for name, info in dataset_info.items():
+        df = pd.read_csv(data_path + '/' + name, sep="\t")
+            
+        if 'train' in name:
+            train, val = train_test_split(df, test_size = 0.2, stratify=df.loc[:, info[0][0]], random_state=42)
+            train.reset_index(drop=True).to_csv(path + '/' + name[:-4] + '_[TRAIN]' + '.tsv', header=info[1], sep="\t")
+            val.reset_index(drop=True).to_csv(path + '/' + name[:-4] + '_[VAL]' + '.tsv', header=info[1], sep="\t")
+            
+        elif 'test' in name and 'label' in name:
+            df.to_csv(path + '/' + name[:-4] + '_[TEST]' + '.tsv', header=info[1], sep="\t")
 
 def change_parameter_seeds(args):
     #### construction
@@ -16,3 +83,7 @@ def train(dataset_config=str(), device=int(), output_path=str(), parameter_confi
     
     os.system(code_line)
 
+def average(folds_number=int(), log_path=str(), models=list()):
+    ''' Average cross validation results and copy all results to local repo '''
+    #### construction
+    return
