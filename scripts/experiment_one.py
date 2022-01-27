f777d9c1034c0f9f0510abf060f77f90a2d71724
AngelFelipeMP <angel.magnossao@gmail.com>
2022-01-27 16:33:44 +0000

    "refactory code experiment_one.py"

--- scripts/experiment_one.py
+++ scripts/experiment_one.py
@@ -1,5 +1,4 @@
-# from utils import split_data, change_parameter_seeds, train, average
-from utils import process_data, data_acquisition
+from utils import process_data, data_acquisition, train
 import os
 import pandas as pd
 import numpy as np
@@ -8,7 +7,6 @@ from iterstrat.ml_stratifiers import MultilabelStratifiedKFold
 
 folds_number = 2
 experiment = 'E1'
-models = ['mtl', 'stl_exist', 'stl_detoxis']
 device = 0  #gpu 0 / cpu 1
 source_data_path = '/content/drive/MyDrive/Code/MTL_2021/Data'
 data_path = '/content/data'
@@ -19,55 +17,50 @@ config_path = repo_path + '/config/' + experiment
 dataset_config = config_path + '_data_'
 parameter_config = config_path + '_parameter_'
 
+tasks = {'DETOXIS':{'text':'comment','label':'toxicity'},
+            'EXIST':{'text':'text','label':'task1'}}
+
+
 # if __name__ == '__main__':
 
 # install dependencies
 os.system('pip3 install --user -r ' + repo_path + '/machamp/requirements.txt')
 
 # grab data from drive
-data_acquisition(source_data_path, data_path, ['EXIST','DETOXIS'])
+data_acquisition(source_data_path, data_path, tasks.keys())
 
-# process data for machamp standards
-detoxis_file, detoxis_stratify_col = process_data(data_path, 'DETOXIS', 'comment', 'toxicity')
-exist_file, exist_stratify_col = process_data(data_path, 'EXIST', 'text', 'task1')
+# create objs to create the k folds 
+simple_kfold = StratifiedKFold(n_splits=folds_number, random_state=42, shuffle=True)
+multi_label_kfold = MultilabelStratifiedKFold(n_splits=folds_number, random_state=42, shuffle=True)
 
-print(detoxis_stratify_col)
-print(exist_stratify_col)
-
-#TODO comment the code below
-#TODO check if the code are smaller as possible
-#TODO check if it is batter create a function 
-# create skf object to stratify the data split
-
-df_detoxis = pd.read_csv(data_path + '/' + detoxis_file, header=None, index_col=0, sep="\t").reset_index(drop=True)
-df_exist = pd.read_csv(data_path + '/' + exist_file, header=None, index_col=0, sep="\t").reset_index(drop=True)
-
-kfold = StratifiedKFold(n_splits=folds_number, random_state=42, shuffle=True)
-strat_kfold = MultilabelStratifiedKFold(n_splits=folds_number, random_state=42, shuffle=True)
-
-for data_exist, data_detoxis in zip(strat_kfold.split(np.zeros(len(df_exist)), df_exist.iloc[:,exist_stratify_col]), kfold.split(np.zeros(len(df_detoxis)), df_detoxis.iloc[:, detoxis_stratify_col])):
+#TODO test the code
+#TODO may comment the code abouve
+# process data for machamp standards & add info to data/taks dictionary
+for task in tasks.keys():
+    file, stratify_col = process_data(data_path, task, tasks[task]['text'], tasks[task]['label'])
+    df = pd.read_csv(data_path + '/' + file, header=None, index_col=0, sep="\t").reset_index(drop=True)
+    kfold = multi_label_kfold if len(stratify_col) > 1 else simple_kfold
     
-    df_detoxis.iloc[data_detoxis[0]].reset_index(drop=True).to_csv(data_path + '/' + detoxis_file.split('_')[0] + '_processed' + '_[TRAIN]' + '.tsv', header=None, sep="\t") 
-    df_detoxis.iloc[data_detoxis[1]].reset_index(drop=True).to_csv(data_path + '/' + detoxis_file.split('_')[0] + '_processed' + '_[VAL]' + '.tsv', header=None, sep="\t")
-    
-    df_exist.iloc[data_exist[0]].reset_index(drop=True).to_csv(data_path + '/' + exist_file.split('_')[0] + '_processed' + '_[TRAIN]' + '.tsv', header=None, sep="\t") 
-    df_exist.iloc[data_exist[1]].reset_index(drop=True).to_csv(data_path + '/' + exist_file.split('_')[0] + '_processed' + '_[VAL]' + '.tsv', header=None, sep="\t")
+    tasks[task]['file'] = file
+    tasks[task]['stratify_col'] = stratify_col
+    tasks[task]['df'] = df
+    tasks[task]['kfold'] = kfold.split(np.zeros(len(df)), df.iloc[:, stratify_col])
 
-    print(data_exist)
-    print(data_detoxis)
-    print('#######################')
+# Save data folds and Train the models
+split_sequence = tasks.keys()
+for idxs in zip(*[tasks[data]['kfold'] for data in split_sequence]):
+    for idx,task in zip(idxs, split_sequence):
+        tasks[task]['df'].iloc[idx[0]].reset_index(drop=True).to_csv(data_path + '/' + tasks[task]['file'].split('_')[0] + '_processed' + '_[TRAIN]' + '.tsv', header=None, sep="\t")
+        tasks[task]['df'].iloc[idx[1]].reset_index(drop=True).to_csv(data_path + '/' + tasks[task]['file'].split('_')[0] + '_processed' + '_[VAL]' + '.tsv', header=None, sep="\t")
     
-    #TODO add the below part to the code
-    # split_data(data_path, train_index, test_index, 0.2, stratify_head_exist)
+    for model in ['mtl'] + ['stl_' + tasks.lower() for tasks in tasks.keys()]:
+        output_path = experiment + '/' + model 
+        train(
+            dataset_config + model + '_config.json',
+            device,
+            output_path,
+            parameter_config + 'config.json')
 
-    # change_parameter_seeds(parameter_config + 'config.json')
-    # for model in models:
-    #     output_path = experiment + '/' + model 
-    #     train(
-    #         dataset_config + model + '_config.json',
-    #         device,
-    #         output_path,
-    #         parameter_config + 'config.json')
 
 
 #TODO add the avg func
